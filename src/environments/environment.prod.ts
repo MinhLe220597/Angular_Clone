82c8bfa8ff2bfa6384db1f7706da7a67863d3285
MinhLe220597 <55128267+MinhLe220597@users.noreply.github.com>
2024-10-02 22:59:25 +0700

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  coreApiUrl: 'http://localhost:5175/api',
+  mainApiUrl: 'http://localhost:5051/api',
+  learnVocabApiUrl: 'http://192.168.0.176:3004/api',
+};
+
+// export const urlAPI = 'http://192.168.0.176:3003/';
